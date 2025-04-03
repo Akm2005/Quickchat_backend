104a59a46a4da73c697ba3a874088997afadcfca
Aman shvx <amishra@shvx.in>
2025-04-03 11:25:39 +0530

    add database document

---
+++ database/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,  
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    password TEXT NOT NULL,
+    profile_image TEXT
+);
